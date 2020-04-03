8eb8b98a980857d06683fbcd10b88720bff8a46f
fritzgt <tuanis@live.com>
2020-04-03 09:35:07 +0300

    removed Pods directory

--- iUnitTests/CalculatorTests.swift
+++ iUnitTests/CalculatorTests.swift
@@ -1,5 +1,5 @@
 //
-//  iUnitTests.swift
+//  CalculatorTests.swift
 //  iUnitTests
 //
 //  Created by FGT MAC on 4/3/20.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class iUnitTests: XCTestCase {
+class CalculatorTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -25,7 +25,7 @@ class iUnitTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
