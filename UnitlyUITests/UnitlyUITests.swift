5b2b90b6ef24c1d01eb6fd66f02c9528c563a7df
fritzgt <tuanis@live.com>
2020-04-04 11:44:04 +0300

    working on the bottom textField tests

--- UnitlyUITests/UnitlyUITests.swift
+++ UnitlyUITests/UnitlyUITests.swift
@@ -14,21 +14,17 @@ class UnitlyUITests: XCTestCase {
     func testDistance() {
         
         
-        
         let app = XCUIApplication()
-        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .textField).element.tap()
+        app.activate()
+        
+        let topTextField = app.textFields["ViewController.topTextField"]
+        topTextField.tap()
+        topTextField.typeText("1")
+        
         app.toolbars.matching(identifier: "Toolbar").buttons["Done"].tap()
         
         
-//        let app = XCUIApplication()
-//
-//        let topField = app.textFields["Top Text Field"]
-//        topField.tap()
-//        topField.typeText("1")
-//
-//
-//        let doneButton = app.toolbars.matching(identifier: "Toolbar").buttons["Done"]
-//        doneButton.tap()
-
+        
+        
     }
 }
